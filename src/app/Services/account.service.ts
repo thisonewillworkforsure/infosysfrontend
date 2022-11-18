5d93d4397364081dd9ce8daa5506067a668c5f5b
thisonewillworkforsure <ben6228@yahoo.com>
2022-11-17 22:49:57 -0500

    added a service

---
+++ src/app/Services/account.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Account } from '../Models/Account';
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  baseUrl : any = "http://localhost:8081/accounts";
+
+  constructor(private httpClient : HttpClient) { }
+
+  getAllAccount() : Observable<Account[]>{
+    return this.httpClient.get<Account[]>(this.baseUrl);
+  }
+
+  createAccount(account : Account) : Observable<Account>{
+     return this.httpClient.post<Account>(this.baseUrl,account);
+  }
+
+  updateAccount(account : Account) : Observable<Account>{
+    return this.httpClient.put<Account>(this.baseUrl,account);
+ }
+
+  getOneAccount(account : Account) : Observable<Account>{
+    return this.httpClient.post<Account>(this.baseUrl,account);
+  }
+
+  
+}
